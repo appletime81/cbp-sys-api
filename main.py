c66755759e8e37be5b5f7d7c2099aee2c91a2138
appletime81 <itrimsl8696@gmail.com>
2023-01-17 05:13:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -91,6 +91,47 @@ async def generateInvoiceWKMasterInvoiceWKDetailInvoiceMasterInvoiceDetail(
     return {"message": "success"}
 
 
+@app.get(ROOT_URL + "/getInvoiceWKMaster&InvoiceWKDetail/{urlCondition}")
+async def searchInvoiceWKMaster(
+    request: Request,
+    urlCondition: str,
+    db: Session = Depends(get_db),
+):
+    getResult = []
+
+    # get InvoiceWKMaster
+    InvoiceWKMasterDataList = await service.getInvoiceWKMaster(
+        request, urlCondition, db
+    )
+    InvoiceWKMasterDictDataList = [
+        orm_to_pydantic(InvoiceWKMasterData, InvoiceWKMasterSchema).dict()
+        for InvoiceWKMasterData in InvoiceWKMasterDataList
+    ]
+
+    # get InvoiceWKDetail
+    for InvoiceWKMasterDictData in InvoiceWKMasterDictDataList:
+        InvoiceWKDetailDataList = await service.getInvoiceWKDetail(
+            request, f"WKMasterID={InvoiceWKMasterDictData.get('WKMasterID')}", db
+        )
+        InvoiceWKDetailDictDataList = [
+            orm_to_pydantic(InvoiceWKDetailData, InvoiceWKDetailSchema).dict()
+            for InvoiceWKDetailData in InvoiceWKDetailDataList
+        ]
+        newInvoiceWKDetailDictDataList = []
+        for InvoiceWKDetailDictData in InvoiceWKDetailDictDataList:
+            InvoiceWKDetailDictData.pop("WKDetailID")
+            newInvoiceWKDetailDictDataList.append(InvoiceWKDetailDictData)
+        InvoiceWKDetailDictDataList = newInvoiceWKDetailDictDataList
+        # generate InvoiceMaster & InvoiceDetail result
+        getResult.append(
+            {
+                "InvoiceWKMaster": InvoiceWKMasterDictData,
+                "InvoiceWKDetail": InvoiceWKDetailDictDataList,
+            }
+        )
+    return getResult
+
+
 @app.get(ROOT_URL + "/getInvoiceMaster&InvoiceDetailStream/WKMasterID={WKMasterID}")
 async def getInvoiceMasterInvoiceDetailStram(
     request: Request,
@@ -226,20 +267,22 @@ async def getInvoiceMasterInvoiceDetailStram(
         DueDate = InvoiceWKMasterDictData["DueDate"]
         PartyName = InvoiceWKMasterDictData["PartyName"]
         IsPro = InvoiceWKMasterDictData["IsPro"]
-        InvoiceMasterDictData = {
-            "InvMasterID": InvMasterID,
-            "WKMasterID": WKMasterID,
-            "InvoiceNo": InvoiceNo,
-            "PartyName": PartyName,
-            "SupplierName": SupplierName,
-            "SubmarineCable": SubmarineCable,
-            "WorkTitle": WorkTitle,
-            "ContractType": ContractType,
-            "IssueDate": IssueDate,
-            "DueDate": DueDate,
-            "Status": "",
-            "IsPro": IsPro,
-        }
+        InvoiceMasterDictDataList = [
+            {
+                "InvMasterID": InvMasterID + 1,
+                "WKMasterID": WKMasterID,
+                "InvoiceNo": InvoiceNo,
+                "PartyName": PartyName,
+                "SupplierName": SupplierName,
+                "SubmarineCable": SubmarineCable,
+                "WorkTitle": WorkTitle,
+                "ContractType": ContractType,
+                "IssueDate": IssueDate,
+                "DueDate": DueDate,
+                "Status": "",
+                "IsPro": IsPro,
+            }
+        ]
 
         # Step2. generate InvoiceDetail
         InvoiceDetailDictDataList = []
@@ -264,131 +307,10 @@ async def getInvoiceMasterInvoiceDetailStram(
 
     print(len(InvoiceDetailDictDataList))
     streamResponse = {
-        "InvoiceWKMaster": InvoiceWKMasterDictData,
-        " InvoiceDetail": InvoiceDetailDictDataList,
+        "InvoiceMaster": InvoiceMasterDictDataList,
+        "InvoiceDetail": InvoiceDetailDictDataList,
     }
     return streamResponse
 
 
-@app.get(ROOT_URL + "/getInvoiceWKMaster&InvoiceWKDetail/{urlCondition}")
-async def searchInvoiceWKMaster(
-    request: Request,
-    urlCondition: str,
-    db: Session = Depends(get_db),
-):
-    getResult = []
-
-    # get InvoiceWKMaster
-    InvoiceWKMasterDataList = await service.getInvoiceWKMaster(
-        request, urlCondition, db
-    )
-    InvoiceWKMasterDictDataList = [
-        orm_to_pydantic(InvoiceWKMasterData, InvoiceWKMasterSchema).dict()
-        for InvoiceWKMasterData in InvoiceWKMasterDataList
-    ]
-
-    # get InvoiceWKDetail
-    for InvoiceWKMasterDictData in InvoiceWKMasterDictDataList:
-        InvoiceWKDetailDataList = await service.getInvoiceWKDetail(
-            request, f"WKMasterID={InvoiceWKMasterDictData.get('WKMasterID')}", db
-        )
-        InvoiceWKDetailDictDataList = [
-            orm_to_pydantic(InvoiceWKDetailData, InvoiceWKDetailSchema).dict()
-            for InvoiceWKDetailData in InvoiceWKDetailDataList
-        ]
-        newInvoiceWKDetailDictDataList = []
-        for InvoiceWKDetailDictData in InvoiceWKDetailDictDataList:
-            InvoiceWKDetailDictData.pop("WKDetailID")
-            newInvoiceWKDetailDictDataList.append(InvoiceWKDetailDictData)
-        InvoiceWKDetailDictDataList = newInvoiceWKDetailDictDataList
-        # generate InvoiceMaster & InvoiceDetail result
-        getResult.append(
-            {
-                "InvoiceWKMaster": InvoiceWKMasterDictData,
-                "InvoiceWKDetail": InvoiceWKDetailDictDataList,
-            }
-        )
-    return getResult
-
-
 # -------------------------------------------------------------------------------------------------------------------------------------
-
-
-# ------------------------------ BillMaster and  BillDetail ------------------------------
-@app.post(
-    f"{ROOT_URL}/generateBillMaster&BillDetail",
-)
-async def generateBillMasterAndBillDetail(
-    request: Request,
-    db: Session = Depends(get_db),
-):
-    # get condition
-    invoice_data = await request.json()
-    WKMasterID = invoice_data["WKMasterID"]
-    DueDate = invoice_data["DueDate"]
-    if not WKMasterID or not DueDate:  # check condition
-        return {"message": "please input WKMasterID and DueDate"}
-    dict_condition = {"WKMasterID": WKMasterID}
-    url_condition = convert_dict_condition_to_url(dict_condition)
-
-    # get IsPro status from InvoiceWKMaster
-    InvoiceWKMasterData = await service.getInvoiceWKMaster(request, url_condition, db)
-    if not InvoiceWKMasterData:  # check InvoiceWKMasterData if exist
-        return {"message": "no InvoiceWKMasterData"}
-    IsPRo = InvoiceWKMasterData.IsPro
-    SubmarineCable = InvoiceWKMasterData.SubmarineCable
-
-    # get all InvoiceDetail by "InvoiceWKMasterID"
-    InvoiceDetailDataList = await service.getInvoiceDetail(request, url_condition, db)
-
-    # convert InvoiceDetailDataList to dataframe format
-    dfInvoiceDetailDataList = [
-        pd.DataFrame(InvoiceDetailData.__dict__, index=[0])
-        for InvoiceDetailData in InvoiceDetailDataList
-    ]
-    dfInvoiceDetailData = dflist_to_df(dfInvoiceDetailDataList)
-
-    # let dfInvoiceDetailData group by PartyName
-    dfInvoiceDetailDataGroupByPartyName = (
-        dfInvoiceDetailData.groupby(["PartyName"])["BillMilestone"]
-        .apply(list)
-        .reset_index(name="BillMilestoneList")
-    )
-    dfInvoiceDetailDataGroupByPartyName.to_csv(
-        "dfInvoiceDetailDataGroupByPartyName.csv"
-    )
-
-    # generate BillMaster
-    BillMasterDictDataList = []
-    for index, row in dfInvoiceDetailDataGroupByPartyName.iterrows():
-        BillMasterDictData = {}
-        subBillingNoString = ""
-        for BillMilestone in list(set(row["BillMilestoneList"])):
-            subBillingNoString += f"{BillMilestone}-"
-
-        BillingNo = f'{SubmarineCable}-CBP-{row["PartyName"]}-{subBillingNoString[:-1]}'
-        PartyName = row["PartyName"]
-        CreateDate = convert_time_to_str(datetime.now())
-        Status = "Initial"
-        BillMasterDictData.update(
-            {
-                "BillingNo": BillingNo,
-                "PartyName": PartyName,
-                "CreateDate": CreateDate,
-                "DueDate": DueDate,
-                "Status": Status,
-                "IsPro": IsPRo,
-            }
-        )
-        BillMasterPydanticData = BillMasterSchema(**BillMasterDictData)
-        addBillMasterResponse = await service.addBillMaster(
-            request, BillMasterPydanticData, db
-        )
-        BillMasterDictData["BillMasterID"] = addBillMasterResponse["BillMasterID"]
-        BillMasterDictDataList.append(BillMasterDictData)
-    pprint(BillMasterDictDataList)
-
-    return {"message": "success"}
-
-
-# ----------------------------------------------------------------------------------------
