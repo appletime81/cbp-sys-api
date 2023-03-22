21dd309320ee8cab1ca048d4c16fa5855c91928d
appletime81 <itrimsl8696@gmail.com>
2023-03-22 09:45:20 +0800

    Update app.py

--- service/UploadFile/app.py
+++ service/UploadFile/app.py
@@ -23,4 +23,21 @@ async def uploadfile(
     return {"file_name": file.filename}
 
 
+@router.post("/UploadSignedBillMaster/{urlCondition}")
+async def uploadSignedBillMaster(
+    request: Request, db: Session = Depends(get_db), file: UploadFile = File(...)
+):
+    """
+    urlCondition: BillMasterID = int
+    """
+    # TODO: Update BillMaster URI
+    print(type(file.filename))
+    with open(file.filename, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+    with open(file.filename, "rb") as f:
+        data = f.read()
+    print(data)
+    return {"file_name": file.filename}
+
+
 # -------------------------------------------------------------------------
