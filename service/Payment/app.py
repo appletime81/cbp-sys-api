55c94771d7f82b4185560940eddc7b5dfd579375
appletime81 <itrimsl8696@gmail.com>
2023-04-11 18:51:50 +0800

    Create app.py

---
+++ service/Payment/app.py
@@ -0,0 +1,24 @@
+﻿from fastapi import APIRouter, Request, Depends
+from fastapi.responses import FileResponse
+
+from crud import *
+from get_db import get_db
+from sqlalchemy.orm import Session
+from utils.utils import *
+from utils.orm_pydantic_convert import *
+
+import os
+from copy import deepcopy
+from docxtpl import DocxTemplate, InlineImage
+
+
+router = APIRouter()
+
+
+# ------------------------------- 付款功能 -------------------------------
+@router.post("/InvoiceWKMaster/payment")
+async def paymentForInvWKMaster(
+    request: Request,
+    db: Session = Depends(get_db),
+):
+    pass
