37a782260110c23d14d35c20bd4b40a8fee12809
appletime81 <itrimsl8696@gmail.com>
2023-04-11 16:41:53 +0800

    Update

--- database/engine.py
+++ database/engine.py
@@ -1,6 +1,6 @@
 import configparser
 
-from sqlalchemy import create_engine, MetaData
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
