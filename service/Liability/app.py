572dba4e1ca42f2a867a54591d788bd119d7c7af
appletime81 <itrimsl8696@gmail.com>
2023-04-18 19:27:53 +0800

    Update app.py

--- service/Liability/app.py
+++ service/Liability/app.py
@@ -19,6 +19,7 @@ async def getLiability(
 ):
     crud = CRUD(db, LiabilityDBModel)
     table_name = "Liability"
+    origLiabilityCondition = deepcopy(LiabilityCondition)
 
     EndBool = "Init"
     if "End" in LiabilityCondition:
@@ -34,9 +35,35 @@ async def getLiability(
         LiabilityCondition = convert_url_condition_to_dict(LiabilityCondition)
         sql_condition = convert_dict_to_sql_condition(LiabilityCondition, table_name)
         LiabilityDataList = crud.get_all_by_sql(sql_condition)
+        LiabilityDictDataList = [
+            orm_to_dict(LiabilityData) for LiabilityData in LiabilityDataList
+        ]
+        for i, LiabilityDictData in enumerate(LiabilityDictDataList):
+            if LiabilityDictData["EndDate"] == "NaT":
+                LiabilityDictDataList[i]["EndDate"] = None
+        pprint(LiabilityDictDataList)
     else:
         LiabilityCondition = convert_url_condition_to_dict(LiabilityCondition)
         LiabilityDataList = crud.get_with_condition(LiabilityCondition)
+
+    if EndBool != "Init" and "start" in origLiabilityCondition:
+        # 篩選沒終止的資料
+        if EndBool:
+            LiabilityDictDataList = [
+                LiabilityDictData
+                for LiabilityDictData in LiabilityDictDataList
+                if LiabilityDictData["EndDate"]
+            ]
+        # 篩選有終止的資料
+        if not EndBool:
+            LiabilityDictDataList = [
+                LiabilityDictData
+                for LiabilityDictData in LiabilityDictDataList
+                if not LiabilityDictData["EndDate"]
+            ]
+        return LiabilityDictDataList
+    elif EndBool == "Init" and "start" in origLiabilityCondition:
+        return LiabilityDictDataList
 
     if EndBool != "Init":
         # 篩選沒終止的資料
