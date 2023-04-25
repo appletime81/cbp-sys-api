f9c83b13607ac5094041e1302e323cd27bebf1a5
appletime81 <itrimsl8696@gmail.com>
2023-04-25 18:16:46 +0800

    Update test.py

--- test.py
+++ test.py
@@ -2,10 +2,10 @@ import json
 from pprint import pprint
 
 # load file test1.json
-with open('test1.json') as f:
+with open("test1.json") as f:
     data = json.load(f)
 
 # print data
-InvoiceDetailList = data['InvoiceDetail']
+InvoiceDetailList = data["InvoiceDetail"]
 pprint(InvoiceDetailList)
 print(sum([InvoiceDetail["FeeAmountPost"] for InvoiceDetail in InvoiceDetailList]))
